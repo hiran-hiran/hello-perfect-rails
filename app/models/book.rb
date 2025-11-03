cb9f221ae894d117eb5e3ddca4c123b3c30222e9
hiran-hiran <nozomu.hirata@timee.co.jp>
2025-11-03 21:42:40 +0900

    validate

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,22 @@ class Book < ApplicationRecord
   scope :written_about, ->(word) { where("name like ?", "%#{word}%") }
 
   belongs_to :publisher
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  before_validation :add_lovely_to_cat
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
+
+  private
+    def add_lovely_to_cat
+      self.name = self.name.gsub(/Cat/) do |matched|
+        "lovely #{matched}"
+      end
+    end
 end
